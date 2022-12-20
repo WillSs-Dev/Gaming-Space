43dc24d8a4bf90867d9ce92ca3c6d82556b36906
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-12-20 15:30:18 -0300

    creating slider component

---
+++ src/components/Slider.jsx
@@ -0,0 +1,74 @@
+import {
+  CardMedia,
+  Typography,
+  Card,
+  CardContent,
+  Paper,
+  Box,
+  useTheme,
+  Chip,
+  Avatar,
+} from '@mui/material';
+import { Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { fiveGames } from '../api/mock-responses';
+const { results } = fiveGames;
+
+function Slider() {
+  const { palette } = useTheme();
+
+  return (
+    <Paper sx={{ width: '90vW', p: 5, m: 'auto' }}>
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        slidesPerView={1}
+        navigation={{ clickable: true }}
+        pagination
+        loop>
+        {results.map((game) => (
+          <SwiperSlide key={game.id}>
+            <Card sx={{ display: 'flex' }}>
+              <CardMedia
+                component='img'
+                sx={{ width: 600, heigth: 300, objectFit: 'cover' }}
+                alt={game.name}
+                image={game.background_image}
+              />
+              <Box>
+                <CardContent sx={{ p: 2 }}>
+                  <Typography variant='h5' color={palette.text.primary}>
+                    {game.name}
+                  </Typography>
+                  <Typography variant='body1' color='palette.text.primary'>
+                    Release date: {game.released}
+                  </Typography>
+                  <Chip
+                    avatar={<Avatar>R</Avatar>}
+                    label={`Rating: ${game.rating}`}
+                  />
+                  <Typography variant='body1' color='palette.text.primary'>
+                    Genres
+                  </Typography>
+                  {game.genres.map((genre) => (
+                    <Chip key={genre.id} label={genre.name} />
+                  ))}
+                  <Typography variant='body1' color='palette.text.primary'>
+                    Released on:
+                  </Typography>
+                  {game.parent_platforms.map(({ platform }) => (
+                    <Chip key={platform.id} label={platform.name} />
+                  ))}
+                </CardContent>
+              </Box>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Paper>
+  );
+}
+
+export default Slider;
