711891b6866861914beeacd6791f16abb65bf861
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-12-20 18:53:26 -0300

    icon dictionary

---
+++ src/utils/icons.js
@@ -0,0 +1,23 @@
+import windows from '../images/windows.svg'
+import xbox from '../images/xbox.svg'
+import nSwitch from '../images/nintendo-switch.svg'
+import apple from '../images/apple.svg'
+import macos from '../images/macos.svg'
+import ps from '../images/playstation.svg'
+import android from '../images/android.svg'
+import linux from '../images/linux.svg'
+import web from '../images/web.svg'
+
+const getIcon = {
+  PC: windows,
+  Xbox: xbox,
+  Nintendo: nSwitch,
+  iOS: apple,
+  'Apple Macintosh': macos,
+  PlayStation: ps,
+  Android: android,
+  Linux: linux,
+  Web: web,
+};
+
+export default getIcon;
