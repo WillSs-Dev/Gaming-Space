9606bc21b2be84b8aaf262564a063252e9a867e8
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-12-22 17:49:04 -0300

    improvingn display of game information on slider component

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -9,6 +9,7 @@ import {
   Chip,
   Avatar,
   useMediaQuery,
+  Grid,
 } from '@mui/material';
 import { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -25,7 +26,7 @@ function Slider() {
   const mobile = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
-    <Paper sx={{ p: 3, m: 'auto' }}>
+    <Paper sx={{ p: 3, m: 'auto', borderRadius: '15px' }}>
       <Swiper
         modules={[Navigation, Pagination]}
         slidesPerView={1}
@@ -34,55 +35,76 @@ function Slider() {
         loop>
         {results.map((game) => (
           <SwiperSlide key={game.id}>
-            <Card sx={{ display: mobile ? 'block' : 'flex' }}>
+            <Card
+              sx={{
+                display: mobile ? 'block' : 'flex',
+                px: 5,
+                justifyContent: 'space-evenly',
+              }}>
               <CardMedia
                 component='img'
                 sx={{
                   maxWidth: mobile ? 800 : 600,
                   maxHeight: 300,
                   minHeight: 300,
+                  borderRadius: '15px',
                 }}
                 alt={game.name}
                 image={game.background_image}
               />
               <Box>
                 <CardContent sx={{ p: 2 }}>
-                  <Typography variant='h5' color={palette.text.primary}>
+                  <Typography variant='h4' color={palette.text.primary}>
                     {game.name}
                   </Typography>
                   <Typography variant='body1' color='palette.text.primary'>
                     Release date: {game.released}
                   </Typography>
                   <Chip
+                    sx={{ mt: 1 }}
                     avatar={<Avatar>R</Avatar>}
                     label={`Rating: ${game.rating}`}
                   />
-                  <Typography variant='body1' color='palette.text.primary'>
+                  <Typography
+                    variant='h5'
+                    color='palette.text.primary'
+                    sx={{ mt: 1 }}>
                     Genres
                   </Typography>
-                  {game.genres.map((genre) => (
-                    <Chip
-                      key={genre.id}
-                      label={genre.name}
-                      sx={{ pl: 0.5, m: 0.5 }}
-                    />
-                  ))}
-                  <Typography variant='body1' color='palette.text.primary'>
-                    Released on:
-                  </Typography>
-                  {game.parent_platforms.map(({ platform }) => (
-                    <Chip
-                      key={platform.id}
-                      label={platform.name}
-                      icon={
-                        <img
-                          alt={platform.name}
-                          src={getIcon[platform.name]}
+                  <Grid container>
+                    {game.genres.map((genre) => (
+                      <Grid item>
+                        <Chip
+                          key={genre.id}
+                          label={genre.name}
                           sx={{ pl: 0.5, m: 0.5 }}
                         />
-                      }
-                    />
-                  ))}
+                      </Grid>
+                    ))}
+                  </Grid>
+                  <Typography
+                    variant='h5'
+                    color='palette.text.primary'
+                    sx={{ mt: 1 }}>
+                    Released on:
+                  </Typography>
+                  <Grid container spacing={1}>
+                    {game.parent_platforms.map(({ platform }) => (
+                      <Grid item>
+                        <Chip
+                          key={platform.id}
+                          label={platform.name}
+                          icon={
+                            <img
+                              alt={platform.name}
+                              src={getIcon[platform.name]}
+                              sx={{ pl: 0.5, m: 0.5 }}
+                            />
+                          }
+                        />
+                      </Grid>
+                    ))}
+                  </Grid>
                 </CardContent>
               </Box>
             </Card>
