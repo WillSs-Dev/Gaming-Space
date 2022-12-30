f2b7428b13d042e6d5092cd6d0e136e24e722772
Will-Ss <williansilva3dsi@hotmail.com>
2022-12-30 18:25:16 -0300

    adjusting fetch parameters to new specifications

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-    'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com',
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+    'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
   },
 };
 
@@ -10,10 +10,12 @@ const baseUrl = 'https://rawg-video-games-database.p.rapidapi.com';
 
 const apiKey = process.env.REACT_APP_GAMES_API_PERSONAL_KEY;
 
-const getFivePagesOfGames = (navNumber) => {
-  fetch(`${baseUrl}/games?key=${apiKey}&page=${navNumber}&page_size=150`, options)
+const getNewPage = (navNumber) =>
+  fetch(
+    `${baseUrl}/games?key=${apiKey}&page=${navNumber}&page_size=39`,
+    options
+  )
     .then((response) => response.json())
     .catch((err) => err);
-};
 
-export { getFivePagesOfGames };
+export { getNewPage };
