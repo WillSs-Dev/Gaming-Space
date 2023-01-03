4e13bca3e26ac46b547b474341f55aa746fe2c77
Will-Ss <williansilva3dsi@hotmail.com>
2023-01-03 19:38:49 -0300

    integrating pagination component with requests to the API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,75 @@
 import { Header, SearchBar, Hero, GamesList, Footer } from '../components/';
 // import { thirtyGames as games } from '../api/mock-responses';
 import React, { useEffect, useState } from 'react';
-import { Paper, useMediaQuery, useTheme } from '@mui/material';
+import {
+  Pagination,
+  Paper,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { requestNewGames, selectGamesInCatalog } from '../redux/reducers/catalogSlice';
+import {
+  requestNewGames,
+  selectGamesInCatalog,
+} from '../redux/reducers/catalogSlice';
 
 export default function Home() {
   const [showSlider, setShowSlider] = useState(true);
-  const { catalog: { games } } = useSelector(selectGamesInCatalog);
+  const [pagination, setPagination] = useState(1);
+
+  const {
+    catalog: { games },
+  } = useSelector(selectGamesInCatalog);
+
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down('md'));
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(requestNewGames(1));
-  }, []);
+    dispatch(requestNewGames(pagination));
+  }, [pagination]);
+
   return (
     <Paper
       sx={{
         background:
           'linear-gradient(-180deg, rgba(4,0,30,1) 0%, rgba(11,15,46,1) 15%, rgba(20,28,93,1) 75%, rgba(2,106,208,1) 100%)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
       }}>
       <Header />
       <SearchBar viewSlider={setShowSlider} />
       <Hero showSlider={showSlider} />
-      { games.length ? <GamesList games={games} mobile={mobile} /> : '' }
+      {games.length ? (
+        <Pagination
+          count={99}
+          page={pagination}
+          siblingCount={1}
+          boundaryCount={1}
+          size={'large'}
+          shape='rounded'
+          sx={{ backgroundColor: 'rgba(32,80,148,0.43)', borderRadius: 15 }}
+          onChange={({ target }) => {
+            setPagination(Number(target.innerText));
+          }}
+        />
+      ) : (
+        ''
+      )}
+      {games.length ? <GamesList games={games} mobile={mobile} /> : ''}
+      <Pagination
+        count={99}
+        page={pagination}
+        siblingCount={1}
+        boundaryCount={1}
+        size={'large'}
+        shape='rounded'
+        sx={{ mb: 3, backgroundColor: 'rgba(4,53,108,255)', borderRadius: 15 }}
+        onChange={({ target }) => {
+          setPagination(Number(target.innerText));
+        }}
+      />
       <Footer />
     </Paper>
   );
