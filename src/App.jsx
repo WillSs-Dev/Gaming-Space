281e35100fafd5e34ff977f6867685979b9984de
Will-Ss <williansilva3dsi@hotmail.com>
2023-01-04 19:21:25 -0300

    initializing login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { Routes, Route } from 'react-router';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Library from './pages/Library';
-import FAQ from './pages/FAQ'
+import FAQ from './pages/FAQ';
+import Login from './pages/Login';
 import { useTheme } from '@mui/material';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
     <div style={{ backgroundColor: palette.background.default }}>
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/login' element={<Login />} />
         <Route exact path='/profile' element={<Profile />} />
         <Route exact path='/library' element={<Library />} />
         <Route exact path='/faq' element={<FAQ />} />
