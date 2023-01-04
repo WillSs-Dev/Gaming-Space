8be9144cc7e7878c974d944b46a795204400058d
Will-Ss <williansilva3dsi@hotmail.com>
2023-01-04 15:02:00 -0300

    initiating localstorage integration with ProfileInfo component

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -7,18 +7,35 @@ import {
   Typography,
   useTheme,
 } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function ProfileInfo({ mobile }) {
+  const [images, setImages] = useState();
   const { palette } = useTheme();
   const {
     background: { paper },
   } = palette;
+
+  useEffect(() => {
+    const storedImages = localStorage.getItem('images');
+    if (!storedImages) {
+      const defaultImages = {
+        profile:
+          'https://play-lh.googleusercontent.com/coMv1dl31PCfEs6essJoEUwVryaqKHKQvENdZ_WYpN-PXa8Qfitkg3grQxIVN22W5A',
+        background:
+          'https://wallpapers.com/images/hd/widescreen-starry-night-sky-p7jj72xgabn10f13.jpg',
+      };
+      localStorage.setItem('images', JSON.stringify(defaultImages));
+      setImages(defaultImages);
+    }
+    setImages(JSON.parse(storedImages));
+  }, []);
+
   return (
     <Paper>
       <Paper style={{ maxHeight: 200, overflow: 'hidden' }}>
         <img
-          src='https://wallpapers.com/images/hd/widescreen-starry-night-sky-p7jj72xgabn10f13.jpg'
+          src={images ? images.background : ''}
           alt='user wallpaper'
           width='100%'
         />
@@ -41,7 +58,7 @@ function ProfileInfo({ mobile }) {
               ml: mobile ? 2 : 15,
             }}>
             <Avatar
-              src='https://play-lh.googleusercontent.com/coMv1dl31PCfEs6essJoEUwVryaqKHKQvENdZ_WYpN-PXa8Qfitkg3grQxIVN22W5A'
+              src={images ? images.profile : ''}
               sx={{ width: 100, height: 100, border: `6px solid ${paper}` }}
             />
             <Paper
@@ -68,7 +85,13 @@ function ProfileInfo({ mobile }) {
           </Paper>
         </Paper>
         {/* {EDIT} */}
-        <Paper sx={{ mr: mobile ? 2.5 : 15, py: 2, display: 'flex', alignItems: 'center' }}>
+        <Paper
+          sx={{
+            mr: mobile ? 2.5 : 15,
+            py: 2,
+            display: 'flex',
+            alignItems: 'center',
+          }}>
           <IconButton sx={{ gap: 1 }}>
             <Typography variant='subtitle1'>Edit profile</Typography>
             <Edit />
