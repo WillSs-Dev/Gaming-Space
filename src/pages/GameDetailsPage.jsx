9b61f94a2ccf0c759adcfb2d9621b1a074f74b03
Will-Ss <williansilva3dsi@hotmail.com>
2023-01-05 19:41:56 -0300

    passing all the necessary information for the GameDetails component, both from the API and the Redux state

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -2,8 +2,8 @@ import { Paper, useMediaQuery } from '@mui/material';
 import { Footer, Header, SearchBar, GameDetails } from '../components';
 import React, { useEffect, useState } from 'react';
 import { useTheme } from '@mui/material';
-// import { useParams } from 'react-router-dom';
-// import { getGameById } from '../api';
+import { useParams } from 'react-router-dom';
+import { getGameById } from '../api';
 import { useSelector } from 'react-redux';
 import { selectCurrentGame } from '../redux/reducers/currentGameSlice';
 
@@ -12,13 +12,18 @@ function GameDetailsPage() {
 
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down('md'));
-  // const { id } = useParams();
+  const { id } = useParams();
 
   const currentGame = useSelector(selectCurrentGame);
 
   useEffect(() => {
-    setGame(currentGame.game);
-  }, [currentGame]);
+    const storeGameWithFullInformations = async () => {
+      const requestedGame = await getGameById(id);
+      const fullObj = { ...requestedGame, ...currentGame.game };
+      setGame(fullObj);
+    };
+    storeGameWithFullInformations();
+  }, [currentGame, id]);
 
   return (
     <Paper>
