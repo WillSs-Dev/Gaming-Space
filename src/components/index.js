b54dbe37aba29d344f4212f8a6bce6e162da3e62
Will-Ss <williansilva3dsi@hotmail.com>
2023-01-05 17:49:02 -0300

    exporting new GameDetails component

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
-export {default as Header} from './Header';
-export {default as SearchBar} from './SearchBar';
-export {default as Hero} from './Hero'
-export {default as Slider} from './Slider'
-export {default as GamesList} from './GamesList'
-export {default as GameCard} from './GameCard'
-export {default as Footer} from './Footer'
-export {default as ProfileInfo} from './ProfileInfo'
-export {default as ReviewsList} from './ReviewsList'
-export {default as EditProfile} from './EditProfile'
+export { default as Header } from './Header';
+export { default as SearchBar } from './SearchBar';
+export { default as Hero } from './Hero';
+export { default as Slider } from './Slider';
+export { default as GamesList } from './GamesList';
+export { default as GameCard } from './GameCard';
+export { default as Footer } from './Footer';
+export { default as ProfileInfo } from './ProfileInfo';
+export { default as ReviewsList } from './ReviewsList';
+export { default as EditProfile } from './EditProfile';
+export { default as GameDetails } from './GameDetails';
