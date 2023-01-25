475935da9bd0d49cfb41b0e0f64386abf6e77ace
Will <williansilva3dsi@hotmail.com>
2023-01-24 23:18:37 -0300

    Fix: page break on smaller screems

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -39,6 +39,7 @@ function Profile() {
       sx={{
         background:
           'linear-gradient(-120deg, rgba(4,0,30,1) 0%, rgba(11,15,46,1) 15%, rgba(20,28,93,1) 75%, rgba(2,106,208,1) 120%)',
+          overflowX: 'hidden',
       }}>
       <Header />
       <ProfileInfo mobile={mobile} />
